fad4229128f6daa69b41d7471cdcf3e2ec7cef49
Асылан Crazy987 Ибраев <kamilla.mussina@nu.edu.kz>
2024-03-27 21:20:20 +0500

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿public class HelloWorld
+{	
+	static void PrintArray(int[] myArray, int i = 0)
+	{
+	    int l=myArray.Length;
+		if (i < l)
+		{
+			Console.WriteLine(myArray[l-i-1]);
+			PrintArray(myArray, i + 1);
+		}
+	}
+	
+    public static void Main(string[] args)
+    {
+		int[] myArray = {10, 20, 30, 40, 50};
+		
+		PrintArray(myArray);
+    }
+}
