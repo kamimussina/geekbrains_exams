9f0d86424b5ffd9c61ccbd82375d0c6f7108c456
Асылан Crazy987 Ибраев <kamilla.mussina@nu.edu.kz>
2024-03-27 21:26:15 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+            int Prompt(string message)
+            {
+              Console.Write(message);
+              int result = Convert.ToInt32(Console.ReadLine());
+              return result;
+            }
+            
+            int NaturalNumber(int n, int m)
+            {
+              if (n == m) return n;
+              else Console.Write($"{NaturalNumber(n, m + 1)}, ");
+              return m;
+            }
+            
+            int n = Prompt("Input N: ");
+            int m = Prompt("Input M: ");
+            if (n < 1 || m < 1) 
+            {
+              Console.WriteLine("Error: Enter a natural number!");
+              return;
+            }
+            if (n < m)
+            {
+              Console.WriteLine("Error: N must be more than M. Try again!");
+              return;
+            }
+Console.WriteLine(NaturalNumber(n, m));
