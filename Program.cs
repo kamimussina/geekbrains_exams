f3a94400a1fa9e311ff8fa5af8136f2cfcfb6666
Асылан Crazy987 Ибраев <kamilla.mussina@nu.edu.kz>
2024-03-27 21:01:27 +0500

    Task2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,19 @@
 ﻿Console.Clear();
-            int Prompt(string message)
-            {
-              Console.Write(message);
-              int result = Convert.ToInt32(Console.ReadLine());
-              return result;
-            }
-            
-            int NaturalNumber(int n, int m)
-            {
-              if (n == m) return n;
-              else Console.Write($"{NaturalNumber(n, m + 1)}, ");
-              return m;
-            }
-            
-            int n = Prompt("Input N: ");
-            int m = Prompt("Input M: ");
-            if (n < 1 || m < 1) 
-            {
-              Console.WriteLine("Error: Enter a natural number!");
-              return;
-            }
-            if (n < m)
-            {
-              Console.WriteLine("Error: N must be more than M. Try again!");
-              return;
-            }
-Console.WriteLine(NaturalNumber(n, m));
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
 
+int Akkerman(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+  else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int m = Prompt("Input M: ");
+int n = Prompt("Input N: ");
+
+Console.WriteLine($"A({m},{n}) = {Akkerman(m, n)}");
